0b934786c92bc12495c4e9f31b8ad7674f53d6a7
sebastiankprs <128964620+sebastiankprs@users.noreply.github.com>
2024-04-22 10:26:23 +0200

    Add files via upload

---
+++ can_decoder.py
@@ -0,0 +1,11 @@
+import can
+import cantools
+import cantools.database
+
+dbc = cantools.database.load.file('can_database.dbc')
+
+bus = can.interface.Bus(channel='can0', bustype='socketcan')
+
+for msg in bus:
+    decoded_msg = dbc.decode_message(msg.arbitration_id, msg.data)
+    print(decoded_msg)

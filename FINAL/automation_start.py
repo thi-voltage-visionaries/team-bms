30c3ec5472265a873a7786516970c4ad7c1a4df4
sebastiankprs <128964620+sebastiankprs@users.noreply.github.com>
2024-06-13 12:48:08 +0200

    Update automation_start.py

--- FINAL/automation_start.py
+++ FINAL/automation_start.py
@@ -1,17 +1,23 @@
 import subprocess
 import time
 
+# function to start script
 def start_script(script):
 	process = subprocess.Popen(['python3', script])
 	return process
 
+# path to bms_reader.py
 first_script = '/home/test/Desktop/team-bms/bms_reader.py'
+# path to i2c_display.py
 second_script = '/lcd/i2c_display.py'
 
+# start first script
 first_process = start_script(first_script)
 print(f"{first_script} gestartet mit PID {first_process.pid}")
 
+# wait ten seconds
 time.sleep(10)
 
+# start second script
 second_process = start_script(second_script)
 print(f"{second_script} gestartet mit PID {second_process.pid}")
